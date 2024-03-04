16851d1bb86c9a61a8a2b1df917ad3f702345e53
Lmotap <mota.lucas@proton.me>
2024-03-04 15:09:26 +0100

    Page d'accueil version mobile presque finalisé
    J'ai complété le menu hamburger, l'agencement et la taille des éléments du footer, de la section hero et du header également, j'ai rajouté les icônes du menu hamburger

---
+++ js/app.js
@@ -0,0 +1,87 @@
+/******************
+* Menu hamburger *
+*****************/
+// Récupération des "boutons" responsives
+
+const btnNavShow = document.querySelector("#nav-show");
+const btnNavHide = document.querySelector("#nav-hide");
+
+
+btnNavShow.addEventListener("click", afficherNavigation);
+btnNavHide.addEventListener("click", cacherNavigation);
+
+function afficherNavigation() {
+
+    // Element à manipuler
+    const navMenu = document.querySelector(btnNavShow.dataset.target);
+
+    // Retirer la classe .hide de la liste des classes de l'élément
+    navMenu.classList.remove("hide");
+
+    // Cacher l'icône hamburger
+    btnNavShow.classList.add("hide");
+
+    // Et afficher l'icône "croix de fermeture"
+    btnNavHide.classList.remove("hide");
+}
+
+function cacherNavigation() {
+
+    // Element à manipuler
+    const navMenu = document.querySelector(btnNavHide.dataset.target);
+
+    // Ajouter la classe .hide à la liste des classes de l'élément
+    navMenu.classList.add("hide");
+
+    // Afficher l'icône hamburger
+    btnNavShow.classList.remove("hide")
+
+    // Et cacher l'icône "croix de fermeture"
+    btnNavHide.classList.add("hide");
+
+}
+
+/* Menu déroulants * 
+*******************/
+
+const collapsableMenuItems = document.querySelectorAll(".collapsable-item");
+
+collapsableMenuItems.forEach(element => {
+    element.addEventListener("click", manageCollapsableMenuItem);
+});
+
+function manageCollapsableMenuItem(event) {
+    let collapsable = document.querySelector(event.target.dataset.target);
+    
+    if (collapsable.classList.contains("hide")) {
+        collapsable.classList.remove("hide");
+    } else {
+    collapsable.classList.add("hide");
+    
+    }
+}
+
+
+
+
+function manageCollapsableMenuItem() {
+    let collapsable = document.querySelector(collapsableMenuItem.dataset.target);
+    collapsable.classList.remove("hide");
+}
+
+function manageCollapsableMenuItem() {
+    let collapsable = document.querySelector(collapsableMenuItem.dataset.target);
+
+    if (collapsable.classList.contains("hide")) {
+        collapsable.classList.remove("hide");
+    } else {
+        collapsable.classList.add("hide");
+    }
+}
+
+let collection = [1, 2, 3];
+
+collection.forEach(element => {
+
+});
+
