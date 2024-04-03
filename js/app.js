567a1b6a6052ba071f25d25d7d97d5e4d5f9cf12
Lmotap <mota.lucas@proton.me>
2024-04-03 10:36:33 +0200

    Changement du nom des variables header (HAM)

--- js/app.js
+++ js/app.js
@@ -87,3 +87,13 @@ collection.forEach(element => {
 });
 
 
+
+
+
+
+const icons = document.querySelectorAll('.icon');
+icons.forEach (icon => {  
+icon.addEventListener('click', (event) => {
+    icon.classList.toggle("open");
+    });
+});
