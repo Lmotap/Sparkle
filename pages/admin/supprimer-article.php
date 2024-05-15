7ee1a12cc90c20b116352cb6b307796413095525
Lmotap <mota.lucas@proton.me>
2024-05-15 16:53:41 +0200

    Ignorer les pages/admin

--- pages/admin/supprimer-article.php
+++
@@ -1,9 +0,0 @@
-<?php
-include __DIR__ .  '/../../App/models/Article.php';
-
-$articleId = $_GET['id']; // Récupère l'ID de l'article à partir de la requête GET
-
-$supprimerArticle = Article::delete($articleId);
-
-// Redirigez l'utilisateur vers la page du tableau de bord après la suppression
-header('Location: dashboard.php');
